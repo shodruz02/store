169335ebe68eb0ef000654e08bd4e3a83b93b31d
Shodruz <su7158868@gmail.com>
2024-02-15 11:23:56 +0500

    commit

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Providers
 
 # Register your models here.
+
+@admin.register(Providers)
+class ProvidersAdmin(admin.ModelAdmin):
+    list_display = ["name","address","Phone_number","Email"]
+    list_filter=["name","Phone_number"]
